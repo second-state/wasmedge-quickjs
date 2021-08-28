d9ebc3d0aa325aaf8e55cf4a26bdabc9e48ad69c
csh <458761603@qq.com>
2021-08-28 21:28:22 +0800

    feat: add macro CFUNC_MAGIC_DEF

--- src/quickjs_sys/macros.rs
+++ src/quickjs_sys/macros.rs
@@ -22,6 +22,27 @@ macro_rules! CFUNC_DEF {
 }
 
 #[macro_export]
+macro_rules! CFUNC_MAGIC_DEF {
+    ($name:expr,$func:ident,$len:expr,$magic:expr) => {
+        JSCFunctionListEntry {
+            name: $name.as_ptr() as *const i8,
+            prop_flags: (JS_PROP_WRITABLE | JS_PROP_CONFIGURABLE) as u8,
+            def_type: JS_DEF_CFUNC as u8,
+            magic: $magic,
+            u: JSCFunctionListEntry__bindgen_ty_1 {
+                func: JSCFunctionListEntry__bindgen_ty_1__bindgen_ty_1 {
+                    length: $len,
+                    cproto: JSCFunctionEnum_JS_CFUNC_generic_magic as u8,
+                    cfunc: JSCFunctionType {
+                        generic_magic: Some($func),
+                    },
+                },
+            },
+        }
+    };
+}
+
+#[macro_export]
 macro_rules! JS_CGETSET_DEF {
     ($name:expr,$get:ident,$set:ident) => {
         JSCFunctionListEntry {
