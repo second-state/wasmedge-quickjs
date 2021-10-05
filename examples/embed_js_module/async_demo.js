be738b128ba5b531705eb87790761d8cce3af255
csh <458761603@qq.com>
2021-10-05 15:34:32 +0800

    fix: fix embed_js_module

--- examples/embed_js_module/async_demo.js
+++ examples/embed_js_module/async_demo.js
@@ -4,11 +4,8 @@ async function simple_val (){
     return "abc"
 }
 
-async function wait_simple_val (a){
+export async function wait_simple_val (a){
     let x = await simple_val()
     print("wait_simple_val:",a,':',x)
     return 12345
 }
-
-print(wait_simple_val(1))
-print(wait_simple_val(2))
--- examples/embed_js_module/src/main.rs
+++ examples/embed_js_module/src/main.rs
@@ -5,15 +5,19 @@ fn main() {
 
     let code = r#"
     import('async_demo.js').then((demo)=>{
-        return demo.wait_simple_val()
+        return demo.wait_simple_val(1)
     })
     "#;
 
     let p = ctx.eval_global_str(code);
     println!("before poll:{:?}", p);
+    if let JsValue::Promise(ref p) = p {
+        let v = p.get_result();
+        println!("v = {:?}", v);
+    }
     ctx.promise_loop_poll();
     println!("after poll:{:?}", p);
-    if let JsValue::Promise(p) = p {
+    if let JsValue::Promise(ref p) = p {
         let v = p.get_result();
         println!("v = {:?}", v);
     }
