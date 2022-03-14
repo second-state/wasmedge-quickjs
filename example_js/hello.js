4873a01e28d632550fd7cd4b5495f18f4e2b8b48
2021 <458761603@qq.com>
2022-03-14 19:17:05 +0000

    feat: add process.env

--- example_js/hello.js
+++ example_js/hello.js
@@ -1,8 +1,14 @@
 import * as os from 'os';
 import * as std from 'std';
+import * as process from 'process'
 
 args = args.slice(1);
 print('Hello', ...args);
 setTimeout(() => {
     print('timeout 2s');
 }, 2000);
+
+let env = process.env
+for(var k in env){
+    print(k,'=',env[k])
+}
