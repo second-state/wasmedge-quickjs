a0604851008a6f991fe7ff1daa09a6e4197a1cbe
Michael Yuan <michael@michaelyuan.com>
2022-03-23 01:56:34 +0800

    Update index.js
    Signed-off-by: Michael Yuan <michael@michaelyuan.com>

--- example_js/create-react-app-ssr/server/index.js
+++ example_js/create-react-app-ssr/server/index.js
@@ -85,7 +85,7 @@ async function handle_req(s, req) {
 async function server_start() {
     print('listen 8002...');
     try {
-        let s = new net.WasiTcpServer(8002);
+        let s = new net.WasiTcpServer(8003);
         for (var i = 0; ; i++) {
             let cs = await s.accept();
             handle_client(cs);
