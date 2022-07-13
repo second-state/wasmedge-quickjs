e272a2bd4bcdae9599ddb091fac33ce0ec779a53
2021 <458761603@qq.com>
2022-07-13 16:23:16 +0000

    refactor: refactor streams (use deno code)

--- modules/internal/validators.js
+++ modules/internal/validators.js
@@ -1,4 +1,4 @@
-import { ERR_SOCKET_BAD_PORT, ERR_INVALID_ARG_TYPE, hideStackFrames } from 'internal/errors'
+import { ERR_SOCKET_BAD_PORT, ERR_INVALID_ARG_TYPE, ERR_INVALID_CALLBACK, hideStackFrames } from 'internal/errors'
 
 export function validatePort(port, name = "Port", allowZero = true) {
     if (
@@ -29,6 +29,56 @@ export function validateString(value, name) {
     }
 }
 
+/**
+ * @param {unknown} value
+ * @param {string} name
+ */
+function validateBoolean(value, name) {
+    if (typeof value !== "boolean") {
+        throw new codes.ERR_INVALID_ARG_TYPE(name, "boolean", value);
+    }
+}
+
+
+/**
+ * @param {unknown} signal
+ * @param {string} name
+ */
+export const validateAbortSignal = hideStackFrames(
+    (signal, name) => {
+        if (
+            signal !== undefined &&
+            (signal === null ||
+                typeof signal !== "object" ||
+                !("aborted" in signal))
+        ) {
+            throw new codes.ERR_INVALID_ARG_TYPE(name, "AbortSignal", signal);
+        }
+    },
+);
+
+export const validateObject = hideStackFrames((value, name, options) => {
+    const useDefaultOptions = options == null;
+    const allowArray = useDefaultOptions ? false : options.allowArray;
+    const allowFunction = useDefaultOptions ? false : options.allowFunction;
+    const nullable = useDefaultOptions ? false : options.nullable;
+    if (
+        (!nullable && value === null) ||
+        (!allowArray && Array.isArray(value)) ||
+        (typeof value !== "object" && (
+            !allowFunction || typeof value !== "function"
+        ))
+    ) {
+        throw new ERR_INVALID_ARG_TYPE(name, "Object", value);
+    }
+});
+
+export const validateCallback = hideStackFrames((callback) => {
+    if (typeof callback !== "function") {
+        throw new ERR_INVALID_CALLBACK(callback);
+    }
+});
+
 export default {
-    validatePort, validateFunction, validateString
+    validatePort, validateFunction, validateString, validateObject, validateAbortSignal, validateCallback
 }
