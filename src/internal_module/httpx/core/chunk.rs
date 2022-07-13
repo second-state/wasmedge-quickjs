d25ddbf876d65074ba5b770f3571cf4fa11a997c
2021 <458761603@qq.com>
2022-07-13 16:16:18 +0000

    fix: fix typo

--- src/internal_module/httpx/core/chunk.rs
+++ src/internal_module/httpx/core/chunk.rs
@@ -11,28 +11,23 @@ impl HttpChunk {
     pub fn parse(buf: &[u8]) -> Result<(&[u8], usize), ParseError> {
         let mut c = Cursor::new(buf);
         let mut header = String::new();
-        let n = c.read_line(&mut header).map_err(|e| {
-            println!("header {:?}", e);
-            ParseError::InvalidChunk
-        })?;
+        let n = c
+            .read_line(&mut header)
+            .map_err(|_| ParseError::InvalidChunk)?;
         if n == 0 {
             return Err(ParseError::Pending);
         }
 
-        let len = usize::from_str_radix(header.trim(), 16).map_err(|e| {
-            println!("hex{} {:?}", header.trim(), e);
-            ParseError::InvalidChunk
-        })?;
+        let len = usize::from_str_radix(header.trim(), 16).map_err(|_| ParseError::InvalidChunk)?;
 
         let r_buf = buf.get(n..n + len).ok_or(ParseError::Pending)?;
 
         c.set_position((n + len) as u64);
 
         let mut end = header;
-        let end_n = c.read_line(&mut end).map_err(|e| {
-            println!("end_n {:?}", e);
-            ParseError::InvalidChunk
-        })?;
+        let end_n = c
+            .read_line(&mut end)
+            .map_err(|_| ParseError::InvalidChunk)?;
 
         if end_n == 0 {
             return Err(ParseError::Pending);
