9ea8c6e0f4369d241b1cb26da88dd70fce8775c0
Michael Yuan <michael@michaelyuan.com>
2022-07-21 01:28:26 +0200

    Update image.js
    Signed-off-by: Michael Yuan <michael@michaelyuan.com>

--- example_js/image.js
+++ example_js/image.js
@@ -1,4 +1,4 @@
 import {Image} from 'image';
-let img = new Image('./bird.png');
+let img = new Image(__dirname + '/bird.png');
 let img_luma = img.to_luma();
-img_luma.save_to_file('./bird_luma.png')
+img_luma.save_to_file(__dirname + '/bird_luma.png')
