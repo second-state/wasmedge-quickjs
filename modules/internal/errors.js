31788a2680494cfd3344cd788b8cfd280706ba2d
Puellaquae <shentukeqin@hotmail.com>
2022-09-12 18:59:26 +0800

    feat: some code required by fs

--- modules/internal/errors.js
+++ modules/internal/errors.js
@@ -1,3 +1,11 @@
+import { inspect } from "./util/inspect";
+
+/**
+ * 
+ * @template T
+ * @param {T} fn 
+ * @return {T}
+ */
 export function hideStackFrames(fn) {
     const hidden = "__node_internal_" + fn.name;
     Object.defineProperty(fn, "name", { value: hidden });
@@ -150,6 +158,67 @@ function invalidArgTypeHelper(input) {
     return ` Received type ${typeof input} (${inspected})`;
 }
 
+/**
+ * 
+ * @param {string} val 
+ * @returns {string}
+ */
+function addNumericalSeparator(val) {
+    let res = "";
+    let i = val.length;
+    const start = val[0] === "-" ? 1 : 0;
+    for (; i >= start + 4; i -= 3) {
+        res = `_${val.slice(i - 3, i)}${res}`;
+    }
+    return `${val.slice(0, i)}${res}`;
+}
+
+export class ERR_OUT_OF_RANGE extends RangeError {
+    code = "ERR_OUT_OF_RANGE";
+
+    /**
+     * 
+     * @param {string} str 
+     * @param {string} range 
+     * @param {unknown} input 
+     * @param {boolean} replaceDefaultBoolean 
+     */
+    constructor(
+        str,
+        range,
+        input,
+        replaceDefaultBoolean = false,
+    ) {
+        assert(range, 'Missing "range" argument');
+        let msg = replaceDefaultBoolean
+            ? str
+            : `The value of "${str}" is out of range.`;
+        let received;
+        if (Number.isInteger(input) && Math.abs(input) > 2 ** 32) {
+            received = addNumericalSeparator(String(input));
+        } else if (typeof input === "bigint") {
+            received = String(input);
+            if (input > 2n ** 32n || input < -(2n ** 32n)) {
+                received = addNumericalSeparator(received);
+            }
+            received += "n";
+        } else {
+            received = inspect(input);
+        }
+        msg += ` It must be ${range}. Received ${received}`;
+
+        super(msg);
+
+        const { name } = this;
+        // Add the error code to the name to include it in the stack trace.
+        this.name = `${name} [${this.code}]`;
+        // Access the stack to generate the error message including the error code from the name.
+        this.stack;
+        // Reset the name to the actual name.
+        this.name = name;
+    }
+}
+
 export class ERR_INVALID_ARG_TYPE_RANGE extends RangeError {
     constructor(name, expected, actual) {
         const msg = createInvalidArgType(name, expected);
@@ -159,7 +228,13 @@ export class ERR_INVALID_ARG_TYPE_RANGE extends RangeError {
 }
 
 export class ERR_INVALID_ARG_TYPE extends TypeError {
-    constructor(name, expected) {
+    /**
+     * 
+     * @param {string} name 
+     * @param {string | string[]} expected 
+     * @param {unknown} actual 
+     */
+    constructor(name, expected, actual) {
         const msg = createInvalidArgType(name, expected);
 
         super("ERR_INVALID_ARG_TYPE", `${msg}.${invalidArgTypeHelper(actual)}`);
@@ -382,3 +457,256 @@ export class ERR_INVALID_RETURN_VALUE extends TypeError {
         );
     }
 }
+
+export const captureStackTrace = hideStackFrames(
+    function captureStackTrace(err) {
+        // Error.captureStackTrace is only available in V8
+        const e = new Error();
+        Object.defineProperties(err, {
+            stack: {
+                configurable: true,
+                writable: true,
+                get: () => e.stack
+            }
+        })
+        return err;
+    },
+);
+
+const captureLargerStackTrace = hideStackFrames(
+    function captureLargerStackTrace(err) {
+        Error.captureStackTrace(err);
+
+        return err;
+    },
+);
+
+
+/**
+ * All error instances in Node have additional methods and properties
+ * This export class is meant to be extended by these instances abstracting native JS error instances
+ */
+export class NodeErrorAbstraction extends Error {
+    /**
+     * @type {string}
+     */
+    code;
+
+    /**
+     * 
+     * @param {string} name 
+     * @param {string} code 
+     * @param {string} message 
+     */
+    constructor(name, code, message) {
+        super(message);
+        this.code = code;
+        this.name = name;
+        //This number changes depending on the name of this class
+        //20 characters as of now
+        this.stack = this.stack && `${name} [${this.code}]${this.stack.slice(20)}`;
+    }
+
+    toString() {
+        return `${this.name} [${this.code}]: ${this.message}`;
+    }
+}
+
+/**
+ * @typedef {Object} NodeSystemErrorCtx
+ * @property {string} code
+ * @property {string} syscall
+ * @property {string} message
+ * @property {number} errno
+ * @property {string=} path
+ * @property {string=} dest
+ */
+
+class NodeSystemError extends NodeErrorAbstraction {
+    /**
+     * 
+     * @param {string} key 
+     * @param {NodeSystemErrorCtx} context 
+     * @param {string} msgPrefix 
+     */
+    constructor(key, context, msgPrefix) {
+        let message = `${msgPrefix}: ${context.syscall} returned ` +
+            `${context.code} (${context.message})`;
+
+        if (context.path !== undefined) {
+            message += ` ${context.path}`;
+        }
+        if (context.dest !== undefined) {
+            message += ` => ${context.dest}`;
+        }
+
+        super("SystemError", key, message);
+
+        captureLargerStackTrace(this);
+
+        Object.defineProperties(this, {
+            [kIsNodeError]: {
+                value: true,
+                enumerable: false,
+                writable: false,
+                configurable: true,
+            },
+            info: {
+                value: context,
+                enumerable: true,
+                configurable: true,
+                writable: false,
+            },
+            errno: {
+                get() {
+                    return context.errno;
+                },
+                set: (value) => {
+                    context.errno = value;
+                },
+                enumerable: true,
+                configurable: true,
+            },
+            syscall: {
+                get() {
+                    return context.syscall;
+                },
+                set: (value) => {
+                    context.syscall = value;
+                },
+                enumerable: true,
+                configurable: true,
+            },
+        });
+
+        if (context.path !== undefined) {
+            Object.defineProperty(this, "path", {
+                get() {
+                    return context.path;
+                },
+                set: (value) => {
+                    context.path = value;
+                },
+                enumerable: true,
+                configurable: true,
+            });
+        }
+
+        if (context.dest !== undefined) {
+            Object.defineProperty(this, "dest", {
+                get() {
+                    return context.dest;
+                },
+                set: (value) => {
+                    context.dest = value;
+                },
+                enumerable: true,
+                configurable: true,
+            });
+        }
+    }
+
+    toString() {
+        return `${this.name} [${this.code}]: ${this.message}`;
+    }
+}
+
+/**
+ * 
+ * @param {string} key 
+ * @param {string} msgPrfix 
+ */
+function makeSystemErrorWithCode(key, msgPrfix) {
+    return class NodeError extends NodeSystemError {
+        /**
+         * 
+         * @param {NodeSystemErrorCtx} ctx 
+         */
+        constructor(ctx) {
+            super(key, ctx, msgPrfix);
+        }
+    };
+}
+
+export const ERR_FS_EISDIR = makeSystemErrorWithCode(
+    "ERR_FS_EISDIR",
+    "Path is a directory",
+);
+
+export class ERR_FS_INVALID_SYMLINK_TYPE extends Error {
+    /**
+     * 
+     * @param {string} x 
+     */
+    constructor(x) {
+        super(
+            "ERR_FS_INVALID_SYMLINK_TYPE",
+            `Symlink type must be one of "dir", "file", or "junction". Received "${x}"`,
+        );
+    }
+}
+
+/**
+ * 
+ * @param {number} name 
+ * @returns {[string, string]}
+ */
+function uvErrmapGet(name) {
+    return errorMap.get(name);
+}
+
+const uvUnmappedError = ["UNKNOWN", "unknown error"];
+
+/**
+ * This creates an error compatible with errors produced in the C++
+ * function UVException using a context object with data assembled in C++.
+ * The goal is to migrate them to ERR_* errors later when compatibility is
+ * not a concern.
+ */
+export const uvException = hideStackFrames(
+    /**
+     * 
+     * @param {NodeSystemErrorCtx} ctx 
+     * @returns 
+     */
+    function uvException(ctx) {
+        const { 0: code, 1: uvmsg } = uvErrmapGet(ctx.errno) || uvUnmappedError;
+
+        let message = `${code}: ${ctx.message || uvmsg}, ${ctx.syscall}`;
+
+        let path;
+        let dest;
+
+        if (ctx.path) {
+            path = ctx.path.toString();
+            message += ` '${path}'`;
+        }
+        if (ctx.dest) {
+            dest = ctx.dest.toString();
+            message += ` -> '${dest}'`;
+        }
+
+
+        const err = new Error(message);
+
+        for (const prop of Object.keys(ctx)) {
+            if (prop === "message" || prop === "path" || prop === "dest") {
+                continue;
+            }
+
+            err[prop] = ctx[prop];
+        }
+
+        err.code = code;
+
+        if (path) {
+            err.path = path;
+        }
+
+        if (dest) {
+            err.dest = dest;
+        }
+
+        return captureLargerStackTrace(err);
+    }
+);
