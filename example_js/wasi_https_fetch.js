05b3dbfa12b62dfc2129f125d61c7b8c8b30baf7
csh <458761603@qq.com>
2023-12-25 17:48:41 +0800

    feat: replace wasmedge_rustls_plugin to rustls

--- example_js/wasi_https_fetch.js
+++ example_js/wasi_https_fetch.js
@@ -2,6 +2,7 @@ import { fetch } from 'http'
 
 async function test_fetch() {
     try {
+        print('test_fetch start')
         let r = await fetch('https://httpbin.org/get?id=1')
         print('test_fetch\n', await r.text())
     } catch (e) {
@@ -12,6 +13,7 @@ test_fetch()
 
 async function test_fetch_post() {
     try {
+        print('test_fetch_post start')
         let r = await fetch("https://httpbin.org/post", { method: 'post', 'body': 'post_body' })
         print('test_fetch_post\n', await r.text())
     } catch (e) {
@@ -22,6 +24,7 @@ test_fetch_post()
 
 async function test_fetch_put() {
     try {
+        print('test_fetch_put start')
         let r = await fetch("https://httpbin.org/put",
             {
                 method: "put",
